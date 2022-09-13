bb08343c0af3b074f08f184708a887aece379b65
olaola <ayomidolapo@gmail.com>
2022-09-13 16:31:13 -0700

    function that prints the last digit of a number

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *  jack_bauer - prints every minute of the day of Jack Bauer
- *  starting from 00:00 to 23:59
- *  Return: 0 (Success)
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ * Return: 0 (Success)
  */
 void jack_bauer(void)
 {
