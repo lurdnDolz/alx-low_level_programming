2aa7b0f9cd8288ef7eadb21793be98b0f9e5fb9e
olaola <ayomidolapo@gmail.com>
2022-09-15 11:30:27 -0700

    draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,17 @@
 #include "main.h"
 
 /**
- * print_diagonal - this prints diagonal line n times
- * @n: the number of times diagonal line is printed
+ * print_line - prints straight line n times.
+ * @n: times straight line is printed.
  * Return: no return
  */
-void print_diagonal(int n)
+void print_lines(int n)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(' ');
-		}
-		_putchar(92);
-		if (i < (n - 1))
-			_putchar('\n');
+		_putchar(95);
 	}
 	_putchar('\n');
 }
